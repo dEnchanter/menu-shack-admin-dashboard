5c2c19c41c10c02cbcf3d2c9e5630e69c047a686
dEnchanter <afolabiopaleye@gmail.com>
2024-04-17 13:37:14 +0100

    ..

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { Input } from "../components/ui/input"
-import { Button } from "../components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 import Image from "next/image";
 import { zodResolver } from '@hookform/resolvers/zod'
 import Link from "next/link";
